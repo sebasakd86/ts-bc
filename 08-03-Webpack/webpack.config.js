5029989075d5ab9e4392094d4438fb07d07f78f3
narbaysebas@gmail.com <narbaysebas@gmail.com>
2021-04-04 13:53:29 -0300

    The way showed in the video works for older versions I'm googling to pinpoint the problem, but "webpack serve" doesnt work like that. The server starts, but it's not bundling anything in a file, rather in memory (because it compiles something somewhere)

--- 08-03-Webpack/webpack.config.js
+++ 08-03-Webpack/webpack.config.js
@@ -1,1 +1,41 @@
-module.exports = {};
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/app.ts",
+    output: {
+        filename: "bundle.js", //final file name
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/dist/",
+        clean: true,
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, "./dist"),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 8080,
+    },
+    plugins: [
+        /* ... */
+        // Only update what has changed on hot reload
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devtool: "inline-source-map",
+    //what to do with the files.
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    //which file extensios to add to the bundle
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+};
